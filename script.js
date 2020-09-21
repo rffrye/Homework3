d00d29288c7cf6424070cc50202fb7a5bfbf8626
RFFRYE <rffrye@users.noreply.github.com>
2020-09-21 17:06:52 -0400

    added variables for promtps,  added arays for character types

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Character Arrays by type
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var special = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+//Variables for prompts
+var confirmLength = "";
+var confirmLower;
+var confirmUpper;
+var confirmSpecial;
+var confirmNumber;
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
