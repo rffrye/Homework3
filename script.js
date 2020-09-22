e6d58616d62183ea7d95ac46b637513b1ea48c81
RFFRYE <rffrye@users.noreply.github.com>
2020-09-21 21:42:17 -0400

    added else statement to confirm character lenght.

--- script.js
+++ script.js
@@ -19,22 +19,14 @@ var confirmUpper;
 var confirmSpecial;
 var confirmNumber;
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
 // Prompt to confirm how many characters the user would like in their password
 function generatePassword() {
   var confirmLength = (prompt("How many characters would you like your password to contain?"));
-  if(confirmLength >= 7 || confirmLength <= 128) {
-    alert("Password length must be between 8-128 characters. Try again");
-    var confirmLength = (prompt("How many characters would you like your password to contain?"));
-    }
+    
+    if(confirmLength <= 7 || confirmLength >= 129) {
+        alert("Password length must be between 8-128 characters. Try again");
+        var confirmLength = (prompt("How many characters would you like your password to contain?"));
+    } 
+    
+    else (alert ("Your password will have " + confirmLength + " characters."))
   }
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword)
